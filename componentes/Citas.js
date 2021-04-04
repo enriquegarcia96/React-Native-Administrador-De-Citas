cb51ee77ff29e3a38e1531cc2cdffa4c43148183
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-03 18:37:02 -0600

    se realizo la mejora del diseño y importacion de los iconos

--- componentes/Citas.js
+++ componentes/Citas.js
@@ -1,28 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight,Image,ScrollView } from 'react-native';
 
-const Cita = ({ cita }) => {
+//--- importo los Iconos ---//
+let user = require('../asset/user23.png');
+let dueño = require('.././asset/propie.png');
+let enfermeda = require('.././asset/fiebr.png');
+
+const Cita = ({ item, eliminarPaciente }) => {
+
+    //--- Funciones para el los botones (TouchableHighlight) ---//
+    const dialogoEliminar = ( id ) =>{
+        console.log('eliminando...', id);
+        eliminarPaciente( id );
+    }
+
+
     return ( 
-        <View style={styles.cita}>
-            <View>
+        <ScrollView style={styles.cita}>
+            <View style={styles.row}>
                 <Text style={styles.label}>Paciente: </Text>
-                <Text style={styles.text}>{cita.paciente}</Text>    
+                <Image style={styles.imgIcon} source={user} />
             </View>  
+                <Text style={styles.text}>{item.paciente}</Text>    
+            <View style={styles.row}>
+                <Text style={styles.label}>Propietario: </Text>
+                <Image style={styles.imgIcon} source={dueño} />
+            </View>  
+                <Text style={styles.text}>{item.propietario}</Text>    
+            <View style={styles.row}>
+                <Text style={styles.label}>Sintomas: </Text>
+                <Image style={styles.imgIcon} source={enfermeda} />
+            </View>  
+                <Text style={styles.text}>{item.sintomas}</Text>    
+
             <View>
-                <Text style={styles.label}>Paciente: </Text>
-                <Text style={styles.text}>{cita.propietario}</Text>    
-            </View>  
-            <View>
-                <Text style={styles.label}>Paciente: </Text>
-                <Text style={styles.text}>{cita.sintomas}</Text>    
-            </View>  
-        </View>
+                <TouchableHighlight onPress={ () => dialogoEliminar( item.id ) } style={styles.btnEliminar}>
+                    <Text style={styles.textoEliminar}>Eliminar &times;</Text>
+                </TouchableHighlight>
+            </View>
+        
+        </ScrollView>
      );
 }
  
 const styles = StyleSheet.create({
     cita: {
-        backgroundColor: '#FFF',
+        backgroundColor: '#5c636e',
         borderBottomColor: '#e1e1e1',
         borderStyle: 'solid',
         borderBottomWidth: 2,
@@ -33,12 +56,33 @@ const styles = StyleSheet.create({
     label:{
         fontWeight: 'bold',
         fontSize: 18,
-        marginTop: 20
+        marginTop: 20,
+        color: '#fbfbfb'
     },
     text: {
         fontSize: 18,
+        color: '#f4f7f7'
 
+    },
+    btnEliminar:{
+        padding: 10,
+        backgroundColor: 'red',
+        marginVertical:10
+    },
+    textoEliminar:{
+        color: '#FFF',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    imgIcon: {
+        width: 50,
+        height: 50,
+        
+    },
+    row:{
+        flexDirection:'row'
     }
+    
 })
 
 
